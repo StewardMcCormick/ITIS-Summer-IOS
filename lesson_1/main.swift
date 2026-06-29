ee7fcec73b2cebeaa5745d8ca994768d26a41149
Egor Bessonov <bessonoven@mail.ru>
2026-06-29 23:09:44 +0300

    task 3

--- lesson_1/main.swift
+++ lesson_1/main.swift
@@ -24,3 +24,16 @@ struct Book {
     
     var shortDescription: String { "\(name) — \(author) (\(pageNum) стр.)" }
 }
+
+func genreDespription(genre value: Genre) -> String {
+    switch value {
+    case .Fantasy:
+        return "фантастика — книги о вымышленных мирах и технологиях"
+    case .Classical:
+        return "классика — литература, прошедшая проверку временем"
+    case .Detective:
+        return "детектив — произведения о расследованиях"
+    case .NonFiction:
+        return "нон-фикшн — документальная проза о науке, истории, бизнесе и психологии, расширяющая картину мира без вымысла"
+    }
+}
