b89e0a48488e77010dd8e6e9ab1128557499de32
Egor Bessonov <bessonoven@mail.ru>
2026-06-30 00:23:02 +0300

    task 8

--- lesson_1/main.swift
+++ lesson_1/main.swift
@@ -47,7 +47,7 @@ struct Book: Printable {
         print("Жанр: \(genre.rawValue)")
         print("Дата публикации: \(publishDate)")
         print("Страниц: \(pageNum)")
-        print("Доступность: \(isAvailable ? "доступна" : "недоступна")")
+        print("Доступность: \(isAvailable ? "доступна" : "недоступна")\n")
     }
     
     mutating func issue() -> Void {
@@ -61,105 +61,124 @@ struct Book: Printable {
     }
 }
 
-var books: [Book] = [
-    Book(
-        name: "1984",
-        author: "Джордж Оруэлл",
-        genre: .Fantasy,
-        publishDate: Calendar.current.date(from: DateComponents(year: 1949, month: 6, day: 8))!,
-        pageNum: 328,
-        isAvailable: true
-    ),
-    Book(
-        name: "Преступление и наказание",
-        author: "Фёдор Достоевский",
-        genre: .Classical,
-        publishDate: Calendar.current.date(from: DateComponents(year: 1866, month: 1, day: 1))!,
-        pageNum: 672,
-        isAvailable: false
-    ),
-    Book(
-        name: "Собака Баскервилей",
-        author: "Артур Конан Дойл",
-        genre: .Detective,
-        publishDate: Calendar.current.date(from: DateComponents(year: 1902, month: 4, day: 1))!,
-        pageNum: 256,
-        isAvailable: true
-    ),
-    Book(
-        name: "Sapiens. Краткая история человечества",
-        author: "Юваль Ной Харари",
-        genre: .NonFiction,
-        publishDate: Calendar.current.date(from: DateComponents(year: 2011, month: 1, day: 1))!,
-        pageNum: 512,
-        isAvailable: true
-    ),
-    Book(
-        name: "Дюна",
-        author: "Фрэнк Герберт",
-        genre: .Fantasy,
-        publishDate: Calendar.current.date(from: DateComponents(year: 1965, month: 8, day: 1))!,
-        pageNum: 896,
-        isAvailable: false
-    ),
-    Book(
-        name: "Убийство в Восточном экспрессе",
-        author: "Агата Кристи",
-        genre: .Detective,
-        publishDate: Calendar.current.date(from: DateComponents(year: 1934, month: 1, day: 1))!,
-        pageNum: 256,
-        isAvailable: true
-    ),
-    Book(
-        name: "Анна Каренина",
-        author: "Лев Толстой",
-        genre: .Classical,
-        publishDate: Calendar.current.date(from: DateComponents(year: 1877, month: 1, day: 1))!,
-        pageNum: 864,
-        isAvailable: true
-    ),
-    Book(
-        name: "Thinking, Fast and Slow",
-        author: "Дэниел Канеман",
-        genre: .NonFiction,
-        publishDate: Calendar.current.date(from: DateComponents(year: 2011, month: 10, day: 25))!,
-        pageNum: 499,
-        isAvailable: false
-    ),
-    Book(
-        name: "Властелин Колец: Братство Кольца",
-        author: "Дж. Р. Р. Толкин",
-        genre: .Fantasy,
-        publishDate: Calendar.current.date(from: DateComponents(year: 1954, month: 7, day: 29))!,
-        pageNum: 423,
-        isAvailable: true
-    ),
-    Book(
-        name: "Идиот",
-        author: "Фёдор Достоевский",
-        genre: .Classical,
-        publishDate: Calendar.current.date(from: DateComponents(year: 1869, month: 1, day: 1))!,
-        pageNum: 640,
-        isAvailable: true
-    )
-]
-
-func findBook(byName name: String) -> Book? {
-    for book in books {
-        if book.name == name  {
-            return book
-        }
+class Library {
+    var books: [Book]
+    
+    init(books: [Book]) {
+        self.books = books
     }
     
-    return nil
-}
-
-func findBook(byAuthor author: String) -> Book? {
-    for book in books {
-        if book.author == author  {
-            return book
-        }
+    func findBook(byName name: String) -> Book? {
+        return books.first{ $0.name == name }
     }
     
-    return nil
+    func findBook(byAuthor author: String) -> [Book] {
+        return books.filter{ $0.author == author }
+    }
+    
+    func issueBook(id: Int) -> Void {
+        if (id > books.count - 1 || id < 0) {
+            print("Книги с таким индексом не существует")
+            return
+        }
+        
+        books[id].issue()
+    }
+    
+    func getBooksByGenre(genre: String) -> [Book] {
+        return books.filter{ $0.genre.rawValue.lowercased() == genre.lowercased() }
+    }
+    
+    func getBooksByGenre(genre: Genre) -> [Book] {
+        return books.filter{ $0.genre == genre }
+    }
+    
+    func printAllBooks() -> Void {
+        books.forEach{ $0.PrintInfo() }
+    }
+    
+    static func createTestLibrary() -> Library {
+        return Library(books: [
+            Book(
+                name: "1984",
+                author: "Джордж Оруэлл",
+                genre: .Fantasy,
+                publishDate: Calendar.current.date(from: DateComponents(year: 1949, month: 6, day: 8))!,
+                pageNum: 328,
+                isAvailable: true
+            ),
+            Book(
+                name: "Преступление и наказание",
+                author: "Фёдор Достоевский",
+                genre: .Classical,
+                publishDate: Calendar.current.date(from: DateComponents(year: 1866, month: 1, day: 1))!,
+                pageNum: 672,
+                isAvailable: false
+            ),
+            Book(
+                name: "Собака Баскервилей",
+                author: "Артур Конан Дойл",
+                genre: .Detective,
+                publishDate: Calendar.current.date(from: DateComponents(year: 1902, month: 4, day: 1))!,
+                pageNum: 256,
+                isAvailable: true
+            ),
+            Book(
+                name: "Sapiens. Краткая история человечества",
+                author: "Юваль Ной Харари",
+                genre: .NonFiction,
+                publishDate: Calendar.current.date(from: DateComponents(year: 2011, month: 1, day: 1))!,
+                pageNum: 512,
+                isAvailable: true
+            ),
+            Book(
+                name: "Дюна",
+                author: "Фрэнк Герберт",
+                genre: .Fantasy,
+                publishDate: Calendar.current.date(from: DateComponents(year: 1965, month: 8, day: 1))!,
+                pageNum: 896,
+                isAvailable: false
+            ),
+            Book(
+                name: "Убийство в Восточном экспрессе",
+                author: "Агата Кристи",
+                genre: .Detective,
+                publishDate: Calendar.current.date(from: DateComponents(year: 1934, month: 1, day: 1))!,
+                pageNum: 256,
+                isAvailable: true
+            ),
+            Book(
+                name: "Анна Каренина",
+                author: "Лев Толстой",
+                genre: .Classical,
+                publishDate: Calendar.current.date(from: DateComponents(year: 1877, month: 1, day: 1))!,
+                pageNum: 864,
+                isAvailable: true
+            ),
+            Book(
+                name: "Thinking, Fast and Slow",
+                author: "Дэниел Канеман",
+                genre: .NonFiction,
+                publishDate: Calendar.current.date(from: DateComponents(year: 2011, month: 10, day: 25))!,
+                pageNum: 499,
+                isAvailable: false
+            ),
+            Book(
+                name: "Властелин Колец: Братство Кольца",
+                author: "Дж. Р. Р. Толкин",
+                genre: .Fantasy,
+                publishDate: Calendar.current.date(from: DateComponents(year: 1954, month: 7, day: 29))!,
+                pageNum: 423,
+                isAvailable: true
+            ),
+            Book(
+                name: "Идиот",
+                author: "Фёдор Достоевский",
+                genre: .Classical,
+                publishDate: Calendar.current.date(from: DateComponents(year: 1869, month: 1, day: 1))!,
+                pageNum: 640,
+                isAvailable: true
+            )
+        ])
+    }
 }
