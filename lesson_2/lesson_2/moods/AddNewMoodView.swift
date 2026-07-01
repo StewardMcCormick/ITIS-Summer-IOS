bdbfaea39533ed0dc9987def41101d4be7b7055f
Egor Bessonov <bessonoven@mail.ru>
2026-07-01 03:03:56 +0300

    steps_5-7: adding moods view

---
+++ lesson_2/lesson_2/moods/AddNewMoodView.swift
@@ -0,0 +1,8 @@
+//
+//  AddNewMoodView.swift
+//  lesson_2
+//
+//  Created by Егор Бессонов on 01.07.2026.
+//
+
+import Foundation
---
+++ lesson_2/lesson_2/moods/MoodPicker.swift
@@ -0,0 +1,8 @@
+//
+//  MoodPicker.swift
+//  lesson_2
+//
+//  Created by Егор Бессонов on 01.07.2026.
+//
+
+import Foundation
